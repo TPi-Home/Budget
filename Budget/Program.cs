e35c221a932a36d8f881b39f5abc9e533fcea554
Tyler Pittman <45609608+tpittman1011101@users.noreply.github.com>
2024-06-12 04:57:03 -0400

    adding unit testing

--- Budget/Program.cs
+++ Budget/Program.cs
@@ -24,12 +24,15 @@ namespace BudgetSpreadsheet
             Console.WriteLine("6. Open README.");
             Console.WriteLine("7. Exit.");
             Console.Write("Enter your choice (1, 2, 3, 4, 5, 6 or 7): ");
-            string choice = Console.ReadLine();
-            while (choice != "7")
+            //string choice = Console.ReadLine();
+            Random choice = new Random(); 
+            var unitTest = choice.Next(1, 4);
+            string nchoice = unitTest.ToString();
+            while (nchoice != "7")
             {
-                switch (choice)
+                switch (nchoice)
                 {
-                    case "1":
+                    case "1"://possibly need unit test main and normal main?
                         var result = WorkbookWrite.CreateBlank(workbookFileName, existingBills);
 
                         if (result.Success)
@@ -80,7 +83,9 @@ namespace BudgetSpreadsheet
                 //add sql for storing in program
                 //need to add actual budget portion in addition to expense trackings
                 //hopefully lost at least 50 lines of code with the new classes in testing
-                choice = Console.ReadLine();
+                //choice = Console.ReadLine();
+                unitTest = choice.Next(1, 4);
+                nchoice = unitTest.ToString();
             }
         }
         static void tutorial()//will probably add some modularity here when I work on the gui
