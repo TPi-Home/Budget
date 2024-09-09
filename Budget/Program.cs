2194efd974d13b51490f57ee06b5b9cea516c0b8
Tyler Pittman <tyler.ray.pittman@gmail.com>
2024-09-09 00:13:12 -0400

    displaymenu function

--- Budget/Program.cs
+++ Budget/Program.cs
@@ -12,23 +12,12 @@ namespace BudgetSpreadsheet
 
             var existingBills = new Dictionary<int, List<(string billName, decimal amount, bool isSplit, string autopayStatus)>>();
 
-            // Initial Menu
-            Console.WriteLine("What do you want to do?");
-            Console.WriteLine("1. Create an appropriately named blank spreadsheet in the root directory.\n(This software follows specific naming conventions, such as the current year in the case of the workbook)");
-            Console.WriteLine("2. Add or merge bills into default worksheet.");
-            Console.WriteLine("3. Add formulas to final cells and apply table theme to default sheet.");
-            Console.WriteLine("4. Finalize the current sheet and start a new sheet for data entry.");
-            Console.WriteLine("5. Open tutorial.");
-            Console.WriteLine("6. Open README.");
-            Console.WriteLine("7. Exit.");
-            Console.Write("Enter your choice (1, 2, 3, 4, 5, 6 or 7): ");
-            string? choice = Console.ReadLine();
+            string? choice = String.Empty;
 
-            /*Random choice = new Random(); 
-            var unitTest = choice.Next(1, 4);
-            string nchoice = unitTest.ToString();*/
             while (choice != "7")
             {
+                DisplayMenu();
+                choice = Console.ReadLine();
                 switch (choice)
                 {
                     case "1":
@@ -75,26 +64,12 @@ namespace BudgetSpreadsheet
                         Console.WriteLine("Invalid choice.");
                         break;
                 }
-                Console.WriteLine("What do you want to do?");
-                Console.WriteLine("1. Create an appropriately named blank spreadsheet in the root directory.\n(This software follows specific naming conventions, such as the current year in the case of the workbook)");
-                Console.WriteLine("2. Add or merge bills into default worksheet.");//will be adding a remove option later
-                Console.WriteLine("3. Add formulas to final cells and apply table theme to default sheet.");//Are you sure??
-                Console.WriteLine("4. Finalize the current sheet and start a new sheet for data entry.");//should probably clarify what this means; finalize month
-                Console.WriteLine("5. Open tutorial.");
-                Console.WriteLine("6. Open README.");
-                Console.WriteLine("7. Exit.");
-                Console.Write("Enter your choice (1, 2, 3, 4, 5, 6 or 7): ");// Random rnd = new Random(); int unitTest  = rnd.Next(1, 4);
                 //break up rent and mortgage, taxes (plus adjusting applicable bills to tax rate, prob seperate column), seperate insurance types, subscription audit, income and capital gains, 1099 income, savings info, debt payments, cells for tax season reminders
                 //add delete expensese
                 //check for open file
-                //NEED TO ADD AN OPTION TO BREAK EARLY OR MODIFY NUMBER OF BILLS
-                //NEED TO BE ABLE TO DYNAMICALLY ADD COLUMNS, ENTER NEW FORMULAS WITH EASE (ML?), AND ADJUST BILL SPLITTING 
-                //add sql for storing in program
+                //sql for storing in program
                 //need to add actual budget portion in addition to expense trackings
                 //ensure dependencies use MIT license
-                choice = Console.ReadLine();
-                //unitTest = choice.Next(1, 4);
-                //nchoice = unitTest.ToString();
             }
         }
         static void Tutorial()//will probably add some modularity here when I work on the gui
@@ -107,6 +82,19 @@ namespace BudgetSpreadsheet
         {
             Console.WriteLine("*This software gathers information from the user and builds a spreadsheet based off that information.\n*This is the style of spreadsheet I have used to track my expenses/budget over the last couple years.\n*While I have been content with this model for my personal use, I do believe there will be more to come as I clean it up some.\n" +
                 "Please report bugs directly to me if you know me\n \n \n \nCopyright 2024 Tyler Pittman\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\r\n\r\nTHE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. ");
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Create an appropriately named blank spreadsheet in the root directory.\n(This software follows specific naming conventions, such as the current year in the case of the workbook)");
+            Console.WriteLine("2. Add or merge bills into default worksheet.");
+            Console.WriteLine("3. Add formulas to final cells and apply table theme to default sheet.");
+            Console.WriteLine("4. Finalize the current sheet and start a new sheet for data entry.");
+            Console.WriteLine("5. Open tutorial.");
+            Console.WriteLine("6. Open README.");
+            Console.WriteLine("7. Exit.");
+            Console.Write("Enter your choice (1, 2, 3, 4, 5, 6 or 7): ");
         }
     }
 }
